527b1b0d8f07aa3effb3e55fa68ce1c61fba79fd
unknown <ethemk10k@gmail.com>
2022-01-25 16:03:10 +0300

    This code find number

---
+++ src/sayı/bulma/SayıBulma.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sayı.bulma;
+import java.util.Scanner;
+/**
+ *
+ * @author Monster
+ */
+public class SayıBulma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner s=new Scanner(System.in);
+        
+        int sayi[] = {2,3,17,71,82,90,69,31,42,56,47,74};
+        
+         System.out.print("Bir sayı giriniz: ");
+         
+        for(int i=0;i<sayi.length;i++){
+            
+           
+        }
+        
+    }
+    
+}
